9b51ae70b81ada80bf83441c1877abce9e939363
Ragav <kragavendra1100@gmail.com>
2023-04-09 20:51:06 +0530

    lint fail test - docker file

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def begin():
     except requests.exceptions.RequestException: # just handle exceptions regarding HTTP
         return Response("Something went wrong to retrieve data", mimetype='text/plain')
 
-    lenWord=str(len(session["word"]))Fail
+    lenWord=str(len(session["word"]))
     print("------------------------------\nWord is :",session["word"])
     # print(lenWord,"letters (begin1)")
     score=request.cookies.get("score_cookie")
